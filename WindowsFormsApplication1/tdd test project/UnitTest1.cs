d504d4e1808d4b6c5dfbe6ebeb69990da7c082bb
yairschneid <yairschneid@gmail.com>
2016-12-07 14:32:39 +0200

    testproject created

---
+++ WindowsFormsApplication1/tdd test project/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace tdd_test_project
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
