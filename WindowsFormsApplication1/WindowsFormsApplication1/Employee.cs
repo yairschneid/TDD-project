df0a9a0af67ae248eb919fe8433e8dc6c677abdc
yairschneid <yairschneid@gmail.com>
2016-12-07 14:25:09 +0200

    Emplyee created

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class Employee
+    {
+        public int ID;
+        public string firstName;
+        public string lastName;
+        public int salary;
+
+        public Employee(int ID, string firstName, string lastName, int salary)
+        {
+            this.ID = ID;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.salary = salary;
+        }
+
+        
+    }
+}
