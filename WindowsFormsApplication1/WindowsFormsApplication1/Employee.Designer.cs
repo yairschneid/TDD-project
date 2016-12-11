8db23d2145e2f8398bca6cde259be80a35829f32
yairschneid <yairschneid@gmail.com>
2016-12-11 13:04:22 +0200

    no commit

--- WindowsFormsApplication1/WindowsFormsApplication1/Employee.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Employee.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WindowsFormsApplication1
 {
-    partial class Form1
+    partial class Employee
     {
         /// <summary>
         /// Required designer variable.
@@ -29,66 +29,71 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.firstName = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.lastName = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.salary = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.ID,
-            this.firstName,
-            this.lastName,
-            this.salary});
-            this.dataGridView1.Location = new System.Drawing.Point(76, 99);
+            this.column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGridView1.Location = new System.Drawing.Point(133, 155);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(780, 342);
+            this.dataGridView1.Size = new System.Drawing.Size(811, 272);
             this.dataGridView1.TabIndex = 0;
             // 
-            // ID
+            // column1
             // 
-            this.ID.HeaderText = "ID";
-            this.ID.Name = "ID";
+            this.column1.HeaderText = "ID";
+            this.column1.Name = "column1";
             // 
-            // firstName
+            // Column2
             // 
-            this.firstName.HeaderText = "First Name";
-            this.firstName.Name = "firstName";
+            this.Column2.HeaderText = "First Name";
+            this.Column2.Name = "Column2";
             // 
-            // lastName
+            // Column3
             // 
-            this.lastName.HeaderText = "Last Name";
-            this.lastName.Name = "lastName";
+            this.Column3.HeaderText = "Last Name";
+            this.Column3.Name = "Column3";
             // 
-            // salary
+            // Column4
             // 
-            this.salary.HeaderText = "Salary";
-            this.salary.Name = "salary";
+            this.Column4.HeaderText = "Salary";
+            this.Column4.Name = "Column4";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(76, 29);
+            this.button1.Location = new System.Drawing.Point(133, 67);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(115, 23);
+            this.button1.Size = new System.Drawing.Size(153, 23);
             this.button1.TabIndex = 1;
             this.button1.Text = "Create Table";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // Form1
+            // Employee
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(948, 424);
+            this.ClientSize = new System.Drawing.Size(1019, 420);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "Employee";
+            this.Text = "Employee";
+            this.Load += new System.EventHandler(this.Employee_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
@@ -97,11 +102,10 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
-        private System.Windows.Forms.DataGridViewTextBoxColumn firstName;
-        private System.Windows.Forms.DataGridViewTextBoxColumn lastName;
-        private System.Windows.Forms.DataGridViewTextBoxColumn salary;
+        private System.Windows.Forms.DataGridViewTextBoxColumn column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
         private System.Windows.Forms.Button button1;
     }
 }
-
--- WindowsFormsApplication1/WindowsFormsApplication1/Employee.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Employee.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,7 +10,7 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApplication1
 {
-    class Employee : Form1
+    public partial class Employee : Form
     {
         public int ID;
         public string firstName;
@@ -15,6 +18,26 @@ namespace WindowsFormsApplication1
         public int salary;
         public string[] firstNames = { "Sali", "yoad", "yair", "Itamar", "Alex", "Benjamin", "Shimon", "Sarah", "yosef", "Pedut", "Yulia", "Tehila" };
         public string[] lastNames = { "Ben Mocha", "Wolfstall", "Schneid", "Avrahami", "Shmukler", "Benjamin", "Raviv", "Burman", "yosef", "Shukrun", "Zorin", "Bitarov" };
+        public DataGridView dgv;
+
+      
+        public Employee()
+        {
+            InitializeComponent();
+          //  this.dgv = new DataGridView();
+          //  dgv.Columns.Add("ID", "ID");
+          //  dgv.Columns.Add("firstName", "First Name");
+          //  dgv.Columns.Add("lastName", "Last Name");
+          //  dgv.Columns.Add("salary", "Salary");
+
+        }
+
+        private void Employee_Load(object sender, EventArgs e)
+        {
+
+        }
+
+       
 
         public Employee(int ID, string firstName, string lastName, int salary)
         {
@@ -22,31 +45,30 @@ namespace WindowsFormsApplication1
             this.firstName = firstName;
             this.lastName = lastName;
             this.salary = salary;
-        }
-
-        public Employee()
-        {
+            
         }
 
         public void CreateEmployeeList()
         {
-           for (int i = 0; i < 10000; i++)
+            for (int i = 1; i < 10000; i++)
             {
-                Employee emp = new Employee(randomID(), randomFirst(), randomLast(), randomSalary());
-                DataGridViewRow row = (DataGridViewRow)dgv.Rows[i].Clone();
-                row.Cells[0].Value = emp.ID;
-                row.Cells[1].Value = emp.firstName;
-                row.Cells[2].Value = emp.lastName;
-                row.Cells[3].Value = emp.salary;
-                dgv.Rows.Add(row);
+                // Employee emp = new Employee(randomID(), randomFirst(), randomLast(), randomSalary());
+                // DataGridViewRow row = dgv.Rows.Add();
+                  dataGridView1.Rows.Add(randomID(), randomFirst(), randomLast(), randomSalary());
+                // row.Cells[0].Value = randomID();
+                // row.Cells[1].Value = randomFirst();
+                // row.Cells[2].Value = randomLast();
+                // row.Cells[3].Value = randomSalary();
+                //dgv.Rows.Add(row);
             }
 
         }
 
         public int randomID()
         {
+            int from = 100000000, to = 999999999;
             Random rnd = new Random();
-            return rnd.Next(100000000, 999999999);
+            return rnd.Next(from, to);
         }
 
         public int randomSalary()
@@ -66,5 +88,11 @@ namespace WindowsFormsApplication1
             Random rnd = new Random();
             return lastNames[rnd.Next(0, 12)];
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CreateEmployeeList();
+        }
     }
 }
+
